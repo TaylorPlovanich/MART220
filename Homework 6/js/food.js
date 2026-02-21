e73f7cf56e673e2c370ab82f704976d93e59323a
TaylorPlovanich <taylor.plovanich@umconnect.umt.edu>
2026-02-20 17:57:03 -0700

    HW 6

--- Homework 6/js/food.js
+++ Homework 6/js/food.js
@@ -1,21 +1,20 @@
+// Food class
 class Food {
-    constructor() {
-      // Sets starting position, size, and color for each item
-      this.x = random(50, 750);
-      this.y = random(50, 550);
-      this.size = random(20, 40);
-      this.color = color(random(255), random(100), random(100));
+    constructor(x, y, size, col) {
+      this.x = x;
+      this.y = y;
+      this.size = size;
+      this.col = col;
     }
   
-    // Draws the shapes on the screen
     display() {
-      fill(this.color);
+      fill(this.col);
       noStroke();
-      // Draws the main circle
-      circle(this.x, this.y, this.size); 
-      
-      // Draws a small square detail
-      fill(255, 150); 
-      rect(this.x, this.y, this.size/4, this.size/4);
+  
+      // Rectangle (stem)
+      rect(this.x - this.size / 6, this.y, this.size / 3, this.size);
+  
+      // Circle (top)
+      circle(this.x, this.y, this.size);
     }
   }
--- Homework 6/js/sketch.js
+++ Homework 6/js/sketch.js
@@ -1,54 +1,77 @@
-var foodItems = []; // List to store multiple food objects
-var idleImages = []; // List for idle animation frames
-var runImages = []; // List for running animation frames
-var currentAnim = []; // Keeps track of which animation is active
+// Animation frames
+var idleImages = [];
+var deadImages = [];
+var totalFrames = 10;
 
-var charX = 100; // Character horizontal position
-var charY = 400; // Character vertical position
-var frameIdx = 0; // Current frame being shown
+// Animation state
+var frameIndex = 0;
+var animationSpeed = 8;
+var isMoving = false;
+
+// Character position
+var charX = 300;
+var charY = 300;
+var targetX = 300;
+var targetY = 300;
+var moveSpeed = 3;
+
+// Food array
+var foods = [];
 
 function preload() {
-  // Loads 10 images into the idle array
-  for (var i = 0; i < 10; i++) {
-    idleImages[i] = loadImage("images/Idle" + nf(i, 3) + ".png");
-    // Loads 10 images into the run array with double underscores
-    runImages[i] = loadImage("images/Run__" + nf(i, 3) + ".png");
+  for (var i = 0; i < totalFrames; i++) {
+    var frameNum = nf(i, 3);
+    idleImages[i] = loadImage("images/Idle_" + frameNum + ".png");
+    deadImages[i] = loadImage("images/Dead__" + frameNum + ".png");
   }
 }
 
 function setup() {
   createCanvas(800, 600);
-  currentAnim = idleImages; // Start with the idle animation
 
-  // Loops 5 times to create 5 new food objects
-  for (var i = 0; i < 5; i++) {
-    foodItems.push(new Food());
-  }
+  // Create 5 food objects with different colors, sizes, and positions
+  foods[0] = new Food(100, 150, 40, color(255, 80, 80));
+  foods[1] = new Food(250, 400, 30, color(80, 200, 80));
+  foods[2] = new Food(500, 100, 50, color(80, 150, 255));
+  foods[3] = new Food(650, 450, 35, color(255, 200, 50));
+  foods[4] = new Food(400, 500, 45, color(200, 80, 255));
 }
 
 function draw() {
   background(40);
 
-  // Runs the display function for every food object in the list
-  for (var i = 0; i < foodItems.length; i++) {
-    foodItems[i].display();
+  // Draw all food items
+  for (var i = 0; i < foods.length; i++) {
+    foods[i].display();
   }
 
-  // Switches to run animation and moves x position if keys are held
-  if (keyIsPressed) {
-    currentAnim = runImages; 
-    if (keyIsDown(RIGHT_ARROW)) { charX += 3; }
-    if (keyIsDown(LEFT_ARROW)) { charX -= 3; }
+  // Move character toward target
+  var d = dist(charX, charY, targetX, targetY);
+  if (d > moveSpeed) {
+    charX += (targetX - charX) / d * moveSpeed;
+    charY += (targetY - charY) / d * moveSpeed;
+    isMoving = true;
   } else {
-    // Switches back to idle if no keys are pressed
-    currentAnim = idleImages; 
+    isMoving = false;
   }
 
-  // Draws the current frame of the character
-  image(currentAnim[frameIdx], charX, charY);
+  // Pick animation based on movement
+  var currentAnim = isMoving ? deadImages : idleImages;
 
-  // Changes the frame index every 6 counts to create movement
-  if (frameCount % 6 == 0) {
-    frameIdx = (frameIdx + 1) % currentAnim.length;
+  // Draw character
+  if (currentAnim.length > 0) {
+    image(currentAnim[frameIndex], charX - 50, charY - 80);
   }
+
+  // Advance animation frame
+  if (frameCount % animationSpeed === 0) {
+    frameIndex = (frameIndex + 1) % totalFrames;
+  }
+}
+
+// When user clicks, set new target
+function mouseClicked() {
+  targetX = mouseX;
+  targetY = mouseY;
+  frameIndex = 0;
 }
