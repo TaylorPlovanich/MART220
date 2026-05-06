5904b4c0298387b88a63d13aec4b5aac37cf0a86
TaylorPlovanich <taylor.plovanich@umconnect.umt.edu>
2026-05-06 09:52:58 -0600

    Final Project

--- Final Project/js/sketch.js
+++ Final Project/js/sketch.js
@@ -2,7 +2,6 @@
 // MART 220 - Final Project: Crab Collector
 // =============================================
 
-// --- Global Variables ---
 let player, playerBody;
 let goodFoods = [];   
 let badFoods = [];    
@@ -14,23 +13,19 @@ let gameOver = false;
 let gameWon = false;
 let gameStarted = false;
 
-// Assets
 let idleImgs = [], runImgs = [], deadImgs = [];
 let goodFoodImg, badFoodImg;
 let bgMusic, goodFoodSnd, badFoodSnd;
 
 function preload() {
-  // Character Animations
   for (let i = 0; i <= 9; i++) {
     let num = nf(i, 3);
     idleImgs.push(loadImage('images/Idle' + num + '.png'));
     runImgs.push(loadImage('images/Run__' + num + '.png'));
     deadImgs.push(loadImage('images/Dead' + num + '.png'));
   }
-  // Environment Assets
   goodFoodImg = loadImage('images/crab_meat.png');
   badFoodImg  = loadImage('images/bad_food.png');
-  // Audio Assets
   bgMusic     = loadSound('sounds/background.mp3');
   goodFoodSnd = loadSound('sounds/good_food.mp3');
   badFoodSnd  = loadSound('sounds/bad_food.mp3');
@@ -39,10 +34,9 @@ function preload() {
 function setup() {
   new Canvas(600, 400);
   bgMusic.setLoop(true);
-  initGame(); // Encapsulated setup for easy restarting
+  initGame(); 
 }
 
-// Function to (re)initialize game state
 function initGame() {
   score = 0;
   health = 3;
@@ -54,7 +48,7 @@ function initGame() {
   
   player = new Player(idleImgs, runImgs, deadImgs);
   
-  if (playerBody) playerBody.remove(); // Clean up old physics body
+  if (playerBody) playerBody.remove(); 
   playerBody = new Sprite(300, 200, 30, 50);
   playerBody.collider = 'dynamic';
   playerBody.rotationLock = true;
@@ -77,12 +71,10 @@ function createObstacles() {
 }
 
 function keyPressed() {
-  // Start Game
   if (!gameStarted) {
     gameStarted = true;
     bgMusic.play();
   }
-  // Restart Game
   if ((gameOver || gameWon) && key === 'r') {
     initGame();
     bgMusic.play();
@@ -98,7 +90,6 @@ function draw() {
   }
 
   if (!gameOver && !gameWon) {
-    // Movement Logic
     playerBody.velocity.x = 0;
     playerBody.velocity.y = 0;
     if (keyIsDown(LEFT_ARROW) || keyIsDown(65))  playerBody.velocity.x = -4;
@@ -112,7 +103,6 @@ function draw() {
     player.y = playerBody.pos.y;
     player.update(false);
 
-    // Collectibles
     for (let f of goodFoods) {
       f.display();
       if (f.hits(player)) {
@@ -122,7 +112,6 @@ function draw() {
       }
     }
 
-    // Enemies
     for (let i = badFoods.length - 1; i >= 0; i--) {
       badFoods[i].display();
       if (badFoods[i].hits(player)) {
@@ -132,13 +121,11 @@ function draw() {
         if (health <= 0) { gameOver = true; bgMusic.stop(); }
       }
 
-      // Attack Mechanic (X key)
       if (keyIsDown(88)) {
         let d = dist(player.x, player.y, badFoods[i].x, badFoods[i].y);
         if (d < 100) {
           badFoods[i].takeDamage(); 
           createParticles(badFoods[i].x, badFoods[i].y);
-          // Intentional Audio: High pitched beep for hit
           if (!goodFoodSnd.isPlaying()) {
              goodFoodSnd.rate(2.0); 
              goodFoodSnd.play();
@@ -148,7 +135,6 @@ function draw() {
       }
     }
 
-    // Particles
     for (let i = particles.length - 1; i >= 0; i--) {
       particles[i].update();
       particles[i].show();
@@ -158,7 +144,7 @@ function draw() {
     if (badFoods.length === 0) { gameWon = true; bgMusic.stop(); }
 
   } else {
-    player.update(true); // Death animation
+    player.update(true); 
   }
 
   player.display();
@@ -171,7 +157,6 @@ function createParticles(x, y) {
   for (let i = 0; i < 5; i++) particles.push(new Particle(x, y));
 }
 
-// --- Screen UI Functions ---
 function drawStartScreen() {
   fill(0, 160); rect(0, 0, width, height);
   fill(255); textAlign(CENTER, CENTER);
@@ -204,46 +189,100 @@ function drawGameWon() {
   fill(255); textSize(20); text('Press [R] to Play Again', width / 2, height / 2 + 30);
 }
 
-// =============================================
-// Classes (With Damage Feedback)
-// =============================================
+// --- CLASSES ---
+
+class Particle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.vx = random(-2, 2);
+    this.vy = random(-4, -1);
+    this.alpha = 255;
+  }
+  finished() { return this.alpha < 0; }
+  update() { this.x += this.vx; this.y += this.vy; this.alpha -= 8; }
+  show() {
+    noStroke();
+    fill(255, 150, 0, this.alpha);
+    ellipse(this.x, this.y, 12);
+  }
+}
+
+class Player {
+  constructor(idleImgs, runImgs, deadImgs) {
+    this.x = 300; this.y = 200; this.size = 64;
+    this.idleImgs = idleImgs; this.runImgs = runImgs; this.deadImgs = deadImgs;
+    this.state = 'idle'; this.frameIndex = 0; this.animTimer = 0; this.animSpeed = 6; this.flipped = false;
+  }
+  update(isGameOver) {
+    if (isGameOver) { this.state = 'dead'; this.animate(); return; }
+    let moving = false;
+    if (keyIsDown(LEFT_ARROW) || keyIsDown(65))  { this.flipped = true;  moving = true; }
+    if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) { this.flipped = false; moving = true; }
+    if (keyIsDown(UP_ARROW) || keyIsDown(87))    moving = true;
+    if (keyIsDown(DOWN_ARROW) || keyIsDown(83))  moving = true;
+    this.state = moving ? 'run' : 'idle';
+    this.animate();
+  }
+  animate() {
+    this.animTimer++;
+    if (this.animTimer >= this.animSpeed) {
+      this.animTimer = 0;
+      let imgs = this.currentImages();
+      this.frameIndex = (this.frameIndex + 1) % imgs.length;
+    }
+  }
+  currentImages() {
+    if (this.state === 'run')  return this.runImgs;
+    if (this.state === 'dead') return this.deadImgs;
+    return this.idleImgs;
+  }
+  display() {
+    push(); imageMode(CENTER); translate(this.x, this.y);
+    if (this.flipped) scale(-1, 1);
+    image(this.currentImages()[this.frameIndex], 0, 0, this.size, this.size);
+    pop();
+  }
+}
+
+class Food {
+  constructor(img) {
+    this.img = img; this.size = 40;
+    this.x = random(this.size, width - this.size);
+    this.y = random(this.size, height - this.size);
+    this.timer = 0; this.moveInterval = int(random(120, 300));
+  }
+  update() {
+    this.timer++;
+    if (this.timer >= this.moveInterval) this.moveRandom();
+  }
+  moveRandom() {
+    this.x = random(this.size, width - this.size);
+    this.y = random(this.size, height - this.size);
+    this.timer = 0;
+  }
+  display() { imageMode(CENTER); image(this.img, this.x, this.y, this.size, this.size); }
+  hits(player) { return dist(this.x, this.y, player.x, player.y) < 40; }
+}
 
 class BadFood {
   constructor(img) {
-    this.img = img;
-    this.size = 40;
-    this.x = random(40, 560);
-    this.y = random(40, 360);
-    this.hp = 20;
-    this.shake = 0; // For visual feedback
+    this.img = img; this.size = 40;
+    this.x = random(40, 560); this.y = random(40, 360);
+    this.hp = 20; this.shake = 0;
   }
-
-  takeDamage() {
-    this.hp -= 1;
-    this.shake = 5; // Trigger shake
-  }
-
+  takeDamage() { this.hp -= 1; this.shake = 5; }
   display() {
     push();
     let sx = random(-this.shake, this.shake);
     translate(this.x + sx, this.y);
     imageMode(CENTER);
-    
-    // Hit Flash effect
-    if (this.shake > 0) {
-      tint(255, 0, 0); // Flash Red when hit
-      this.shake *= 0.9;
-    }
+    if (this.shake > 0) { tint(255, 0, 0); this.shake *= 0.9; }
     image(this.img, 0, 0, this.size, this.size);
     pop();
-
-    // Health Bar
     fill(200, 0, 0); rect(this.x - 20, this.y - 30, 40, 5);
     fill(0, 200, 0); rect(this.x - 20, this.y - 30, 40 * (this.hp/20), 5);
   }
-
   hits(player) { return dist(this.x, this.y, player.x, player.y) < 35; }
   moveRandom() { this.x = random(40, 560); this.y = random(40, 360); }
 }
-
-// [Keep your existing Particle, Player, and Food classes below this!]
